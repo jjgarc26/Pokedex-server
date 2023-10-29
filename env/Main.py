87cb6b9af795d9b6fdeea2e14d1750fd0cf1a75b
Jose Garcia <mendoza.joser51@gmail.com>
2023-10-29 14:49:24 -0700

    save work

--- env/Main.py
+++ env/Main.py
@@ -50,3 +50,22 @@ def get_pokemon(pokemon_name):
             mimetype='application/json'
         )
     return response
+@app.route('/regions/<region_name>', methods=['GET'])
+
+def get_region_pokedex(region_name):
+    try:
+        print('Getting pokemon information')
+        request = RequestCall()
+        data = request.get_region_information(region_name)
+        response = app.response_class(
+            response = json.dumps(data),
+            status = 200,
+            mimetype = 'application/json'
+        )
+    except(Exception) as error:
+        response = app.response_class(
+            response=f'error has occurred in "pokedex/{region_name}": {error}',
+            status=400,
+            mimetype='application/json'
+        )
+    return response
--- env/src/Request.py
+++ env/src/Request.py
@@ -10,6 +10,11 @@ class RequestCall:
         self.base_url = os.getenv('POKEDEX_BASE_API')
         self.limit_url = os.getenv('POKEDEX_LIMIT_API')
         self.individual_url = os.getenv('INDIVIDUAL_POKEMON_API')
+        self.kanto_pokedex = os.getenv('KANTO_POKEDEX')
+        self.johto_pokedex = os.getenv('JOHTO_POKEDEX')
+        self.hoenn_pokedex = os.getenv('HOENN_POKEDEX')
+        self.sinnoh_pokedex = os.getenv('SINNOH_POKEDEX')
+
 
     def get_pokemon_list(self, limit):
         try:
@@ -30,7 +35,6 @@ class RequestCall:
             response = requests.get(self.individual_url + pokemon_name)
 
             data = response.json()
-            # print(data['types'][0]['type'])
             
             pokemon_types = [(types['type']['name']) for types in data['types']]
 
@@ -47,3 +51,51 @@ class RequestCall:
             return pokemon_information
         except(Exception) as error:
             return error
+        
+    def get_region_information(self, region):
+        try:
+            print('Getting region information')
+           
+            # sends GET request to retrieve region  pokedex
+            region_pokedex = self.get_region_pokedex(region)
+            print(region_pokedex)
+        
+            # response = requests.get(self.base_url + 'region/' + region)
+            # data = response.json()
+            return region_pokedex
+            
+        except(Exception) as error:
+            return error
+        
+    def get_region_pokedex(self, region):
+        try:
+            print(f'Getting {region} region pokedex')
+
+            region_endpoint = self.get_pokedex_region_endpoint(region)
+            
+            request_url = self.base_url + region_endpoint
+
+            request = requests.get(request_url)
+            data = request.json()
+            print("POKEMON ENTRIES")
+
+            result = [(self.get_pokemon(pokemon['pokemon_species']['name'])) for pokemon in data['pokemon_entries']]
+
+            return result
+        except(Exception) as error:
+            return error
+        
+    def get_pokedex_region_endpoint(self, region):
+        try:
+            print('Getting region endpoint')
+            region_endpoint = {
+                "kanto" : self.kanto_pokedex,
+                "johto": self.johto_pokedex,
+                "hoenn": self.hoenn_pokedex,
+                "sinnoh": self.sinnoh_pokedex
+            }
+            return region_endpoint[region]
+        except(Exception) as error:
+            print("Could not get enpoint")
+            return error
+    
